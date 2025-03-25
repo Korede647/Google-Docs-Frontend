bf6ff17e12033afaf6b06e16260806f96159616c
Korede Anunusor <koredeanunusor@gmai.com>
2025-03-25 12:25:15 +0100

    log in && sign up edits

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LogIn = () => {
-  const [firstName, setFirstName] = useState()
-  const [lastName, setLastName] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [error, setError] = useState("")
 
-  const navigate = useNavigate("/logIn")
+  const navigate = useNavigate()
 
   const validateEmail = (email) => {
       const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -21,16 +20,19 @@ const LogIn = () => {
       setError("")
 
       // check that all fields are filled
-      if (firstName && lastName && email && password && validateEmail(email)) {
+      if (!email || !password || !validateEmail(email)) {
         setError("All fields are required")
+        return
       }
       if(!validateEmail(email)){
          setError("Invalid Email")
+         return
       }
       if(password.length < 6){
         setError("Password must be 6 or more characters")
+        return
       }
-      navigate("/logIn")
+      navigate("/home")
     // }catch(error){
     //   setError("Sign up failed. Please try again")
     // }
@@ -39,47 +41,11 @@ return (
   <div className=" w-100 bg-white p-10 rounded-lg shadow-lg mx-115 my-30">
     <div className="flex flex-wrap gap-5 items-center w-full max-md:max-w-full mb-10">
       <div className="flex flex-wrap flex-1 shrink gap-5 items-center self-stretch my-auto basis-0 min-w-[240px] max-md:max-w-full ">
-        <h2 className="text-blue-900 font-medium text-[20px]">Create an Account</h2>
+        <h2 className="text-blue-900 font-medium text-[20px]">Log In</h2>
       </div>
     </div>
 
-    <div class="grid grid-cols-1 gap-6 mb-10">
-      <div id="input" className="relative">
-        <input
-          type="text"
-          id="floating_outlined"
-          className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
-          placeholder="First name"
-          value={firstName}
-          onChange={() => setFirstName(firstName)}
-        />
-        <label
-          for="floating_outlined"
-          class="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
-          First name
-        </label>
-      </div>
-     
-
-      <div id="input" className="relative">
-        <input
-          type="text"
-          id="floating_outlined"
-          className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
-          placeholder="Last name"
-          value= {lastName}
-          onChange={() => setLastName(lastName)}
-        />
-        <label
-          for="floating_outlined"
-          className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
-          Last name
-        </label>
-      </div>
-      
-
+    <form className="grid grid-cols-1 gap-6 mb-10" onSubmit={handleSignUp}>
       <div id="input" class="relative">
         <input
           type="text"
@@ -87,7 +53,7 @@ return (
           className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
           placeholder="E-mail"
           value= {email}
-          onChange={() => setEmail(email)}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label
           for="floating_outlined"
@@ -95,48 +61,43 @@ return (
         >
           E-mail
         </label>
-        {/* {error && (
-        <p id="input" className="text-red-500 pt-1 text-sm">{emailValid}</p>
-        )} */}
 
       </div>
       
       <div id="input" class="relative">
         <input
-          type="text"
-          id="floating_outlined"
+          type="password"
+          id="floating_outlines"
           className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
           placeholder="Password (6 or more characters)"
-          value= {email}
-          onChange={() => setEmail(email)}
+          value= {password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <label
-          for="floating_outlined"
+          for="floating_outlines"
           className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
         >
           Password
         </label>
         {error && (
-        <p id="input" className="text-red-500 pt-1 text-sm">{error}</p>
+        <p className="text-red-500 pt-1 text-sm">{error}</p>
         )}
       </div>
 
-     
-    </div>
 
     <div className="sm:flex sm:flex-row-reverse flex ">
       <button
-         to="/logIn"
         className="w-fit rounded-lg text-sm px-5 py-3 focus:outline-none h-[50px] border bg-violet-500 hover:bg-violet-600 hover:cursor-pointer focus:bg-violet-700 border-violet-500-violet- text-white focus:ring-4 focus:ring-violet-200 hover:ring-4 hover:ring-violet-100 transition-all duration-300"
-        type="button"
-        onClick={handleSignUp}
+        type="submit"
       >
         <div className="text-center">
-          Sign up
+          Log in
           </div>
       </button>
      
     </div>
+    </form>
+    
   </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-    const [firstName, setFirstName] = useState()
-    const [lastName, setLastName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const [error, setError] = useState("")
 
     const navigate = useNavigate()
@@ -22,14 +22,17 @@ const SignUp = () => {
         setError("")
 
         // check that all fields are filled
-        if (firstName && lastName && email && password && validateEmail(email)) {
+        if (!firstName || !lastName || !email || !password || !validateEmail(email)) {
           setError("All fields are required")
+          return
         }
         if(!validateEmail(email)){
            setError("Invalid Email")
+           return
         }
         if(password.length < 6){
           setError("Password must be 6 or more characters")
+          return
         }
         navigate("/logIn")
       // }catch(error){
@@ -37,6 +40,8 @@ const SignUp = () => {
       // }
     }
   return (
+    <>
+    
     <div className=" w-100 bg-white p-10 rounded-lg shadow-lg mx-115 my-30">
       <div className="flex flex-wrap gap-5 items-center w-full max-md:max-w-full mb-10">
         <div className="flex flex-wrap flex-1 shrink gap-5 items-center self-stretch my-auto basis-0 min-w-[240px] max-md:max-w-full ">
@@ -44,19 +49,19 @@ const SignUp = () => {
         </div>
       </div>
 
-      <div class="grid grid-cols-1 gap-6 mb-10">
-        <div id="input" className="relative">
+      <form className="grid grid-cols-1 gap-6 mb-10" onSubmit={handleSignUp}>
+      <div id="input" className="relative">
           <input
             type="text"
-            id="floating_outlined"
+            id="floating_outlined4"
             className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
             placeholder="First name"
             value={firstName}
-            onChange={() => setFirstName(firstName)}
+            onChange={(e) => setFirstName(e.target.value)}
           />
           <label
-            for="floating_outlined"
-            class="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+            htmlFor="floating_outlined4"
+            className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
           >
             First name
           </label>
@@ -66,14 +71,14 @@ const SignUp = () => {
         <div id="input" className="relative">
           <input
             type="text"
-            id="floating_outlined"
+            id="floating_out"
             className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
             placeholder="Last name"
             value= {lastName}
-            onChange={() => setLastName(lastName)}
+            onChange={(e) => setLastName(e.target.value)}
           />
           <label
-            for="floating_outlined"
+            htmlFor="floating_out"
             className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
           >
             Last name
@@ -81,64 +86,59 @@ const SignUp = () => {
         </div>
         
 
-        <div id="input" class="relative">
+        <div id="input" className="relative">
           <input
             type="text"
-            id="floating_outlined"
+            id="floating_outlin"
             className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
             placeholder="E-mail"
             value= {email}
-            onChange={() => setEmail(email)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label
-            for="floating_outlined"
+            htmlFor="floating_outlin"
             className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
           >
             E-mail
           </label>
-          {/* {error && (
-          <p id="input" className="text-red-500 pt-1 text-sm">{emailValid}</p>
-          )} */}
 
         </div>
         
-        <div id="input" class="relative">
+        <div id="input" className="relative">
           <input
-            type="text"
-            id="floating_outlined"
+            type="password"
+            id="floating_outline"
             className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:outline-none focus:ring-1 focus:ring-violet-300 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary- peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
             placeholder="Password (6 or more characters)"
-            value= {email}
-            onChange={() => setEmail(email)}
+            value= {password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label
-            for="floating_outlined"
+            htmlFor="floating_outline"
             className="text-blue-900 text-bold peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 top-2 z-10 origin-[0] bg-white disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem] rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
           >
             Password
           </label>
           {error && (
-          <p id="input" className="text-red-500 pt-1 text-sm">{error}</p>
+          <p className="text-red-500 pt-1 text-sm">{error}</p>
           )}
         </div>
 
-       
-      </div>
 
       <div className="sm:flex sm:flex-row-reverse flex ">
         <button
-           to="/logIn"
           className="w-fit rounded-lg text-sm px-5 py-3 focus:outline-none h-[50px] border bg-violet-500 hover:bg-violet-600 hover:cursor-pointer focus:bg-violet-700 border-violet-500-violet- text-white focus:ring-4 focus:ring-violet-200 hover:ring-4 hover:ring-violet-100 transition-all duration-300"
-          type="button"
-          onClick={handleSignUp}
+          type="submit"
         >
           <div className="text-center">
             Sign up
             </div>
         </button>
+         </div>
        
-      </div>
+      </form>
     </div>
+    </>
   );
 };
 
